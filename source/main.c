f967e6a7cf7fd15050d487109617970f6ec8d016
ZAWARTO <62563083+ZAWARTO@users.noreply.github.com>
2021-01-24 21:47:12 +0100

    Update main.c

--- source/main.c
+++ source/main.c
@@ -29,14 +29,16 @@ bool posicionar;	//True if the game needs to set up the position of the sprites
 //-----------------------------------------------------
 
 //Data structure that defines the position of the soldier's sprite and its corresponding frame
-typedef struct {
+typedef struct 
+{
 	int x, y; 	// soldier x and y position
 	u16* gfx; 	// gfx of oam
 	u8* gfx_frame;
 } Soldado;
 
 //Data structure that defines the position of the document sprite and its corresponding frame
-typedef struct {
+typedef struct 
+{
 	int x, y;	// document x and y position
 	u16* gfx;	// gfx of oam
 	u8* gfx_frame;
@@ -45,21 +47,29 @@ typedef struct {
 //---------------------------------------------------------------------------------
 // Movement code by survey for the soldier
 //---------------------------------------------------------------------------------
-void movimientoSoldado(Soldado * soldado) {
+void movimientoSoldado(Soldado * soldado) 
+{
 
 	//Assignment of the global key variable to the pressed key function
 	tecla = TeclaPulsada();
 
-	if (tecla == DCHA) {
+	if (tecla == DCHA) 
+	{
 		estadoSoldado = WALK_RIGHT;
 		soldado->x++;
-	} else if (tecla == IZDA) {
+	} 
+	else if (tecla == IZDA) 
+	{
 		estadoSoldado = WALK_LEFT;
 		soldado->x--;
-	} else if (tecla == ABAJO) {
+	} 
+	else if (tecla == ABAJO) 
+	{
 		estadoSoldado = WALK_DOWN;
 		soldado->y++;
-	} else if (tecla == ARRIBA) {
+	} 
+	else if (tecla == ARRIBA) 
+	{
 		estadoSoldado = WALK_UP;
 		soldado->y--;
 	}
@@ -84,8 +94,8 @@ void movimientoSoldado(Soldado * soldado) {
 //---------------------------------------------------------------------------------
 // Code to generate the document
 //---------------------------------------------------------------------------------
-void respawnDocumento(Documento * documento_sprite) {
-
+void respawnDocumento(Documento * documento_sprite) 
+{
 	//The sprite is assigned position 5 of the spritesheet when dividing it into five blocks of 16x16 bits since the fourth slice (partition)
 	//corresponds to the document drawing
 	u8* offset = documento_sprite->gfx_frame + 4 * 16*16;
@@ -105,8 +115,8 @@ void respawnDocumento(Documento * documento_sprite) {
 //---------------------------------------------------------------------------------
 // Code to detect the collision of the soldier sprite and the document
 //---------------------------------------------------------------------------------
-bool collisionDetected(Documento documento_sprite, Soldado soldado) {
-
+bool collisionDetected(Documento documento_sprite, Soldado soldado) 
+{
 	//Sprite dimensions, 5 slices of 16x16
 	int soldadoWidth = 16;
 	int soldadoHeight = 16;
@@ -127,15 +137,15 @@ bool collisionDetected(Documento documento_sprite, Soldado soldado) {
 	if (soldadoLeft < documentoRight && 
 		soldadoRight > documentoLeft && 
 		soldadoTop < documentoBottom && 
-		soldadoBottom > documentoTop) { 
+		soldadoBottom > documentoTop) 
+	{ 
     	return true;
 	}
-
 	return false;
 }
 
-int main() {
-
+int main() 
+{
 	//Initializing the sound memory and loading the file DAZZLER2.MOD which is the music of the game
 	mmInitDefaultMem((mm_addr)soundbank_bin);
 	mmLoad(MOD_DAZZLER2);
@@ -182,7 +192,7 @@ int main() {
 	documento_sprite.gfx = oamAllocateGfx(&oamMain, SpriteSize_16x16, SpriteColorFormat_256Color);
 	documento_sprite.gfx_frame = (u8*)spritesheetTiles;
 	
-	//Initialize interrupts
+	//Initialize interruptions
 	interrupciones();
 
 	//-----------------------------------------------------
@@ -199,39 +209,46 @@ int main() {
 	bool clf = false;
 	pos_pantalla.px=0; //touchpad positions
 	pos_pantalla.py=0;
-
 	//Set the volume to 100/250 and loop it
 	mmEffectVolume(MOD_DAZZLER2, 100);
 	mmStart(MOD_DAZZLER2, MM_PLAY_LOOP);
 
-	while(1) {
-
+	while(1) 
+	{
 		touchRead(&pos_pantalla);
-		while(pos_pantalla.px!=0 || pos_pantalla.py!=0){ // encuesta
+		while(pos_pantalla.px!=0 || pos_pantalla.py!=0)
+		{ 
 			touchRead(&pos_pantalla);
-			if(pos_pantalla.px==0 && pos_pantalla.py==0){
-				if(mmActive()){
+			if(pos_pantalla.px==0 && pos_pantalla.py==0)
+			{
+				if(mmActive())
+				{
 					 mmPause();
 				}
-				else if(!mmActive()){
+				else if(!mmActive())
+				{
 					mmResume();
 				}
 			}
 		}
 		
-		if(posicionar == true){ //Set up the initial position of the sprites every time you press START in the menu
+		if(posicionar == true)
+		{ 
+		//Set up the initial position of the sprites every time you press START in the menu
 		documento_sprite.x = 0;
 		documento_sprite.y = 0;
 		soldado.x = 20;
 		soldado.y = 0;
 		}
 
-		if(estado==INICIO && cl==false){
+		if(estado==INICIO && cl==false)
+		{
 			consoleClear();
 			cl=true;
 		}
 
-		if(estado==INICIO && cl==true){
+		if(estado==INICIO && cl==true)
+		{
 			clp=false;
 			printf("\x1b[01;00H Preparate para comenzar tu ");
 			printf("\x1b[02;00H mision. Eres un soldado");
@@ -240,21 +257,21 @@ int main() {
 			printf("\x1b[05;00H maximo numero de documentos");
 			printf("\x1b[06;00H secretos en el area de");
 			printf("\x1b[07;00H infiltracion.");
-			
 			printf("\x1b[09;00H Date prisa pues tu tiempo");
 			printf("\x1b[10;00H es limitado");
 			printf("\x1b[14;11H Suerte camarada.");
 			MostrarINICIO();
 		}
 
-		if(estado==PRADO && clp==false){
+		if(estado==PRADO && clp==false)
+		{
 			consoleClear();
 			clp=true;	
 			posicionar = false;
 		}
 
-		
-		if(estado==PRADO && clp==true){
+		if(estado==PRADO && clp==true)
+		{
 			cld=false;
 			MostrarPRADO();
 			printf("\x1b[02;05HDocumentos recogidos: %d", documentos);
@@ -262,16 +279,16 @@ int main() {
 			//Show sprites and run sprite move function
 			movimientoSoldado(&soldado);
 			respawnDocumento(&documento_sprite);
-
 		}
 
-		if(estado==DESIERTO && cld==false){
+		if(estado==DESIERTO && cld==false)
+		{
 			consoleClear();
 			cld=true;
 		}
 
-		
-		if(estado==DESIERTO && cld==true){
+		if(estado==DESIERTO && cld==true)
+		{
 			clc=false;
 			MostrarDESIERTO();
 			printf("\x1b[02;05HDocumentos recogidos: %d", documentos);
@@ -279,16 +296,16 @@ int main() {
 			//Show sprites and run sprite move function
 			movimientoSoldado(&soldado);
 			respawnDocumento(&documento_sprite);
-
 		}
 
-		if(estado==CARRETERA && clc==0){
+		if(estado==CARRETERA && clc==0)
+		{
 			consoleClear();
 			clc=1;
 		}
 
-		
-		if(estado==CARRETERA && clc==true){
+		if(estado==CARRETERA && clc==true)
+		{
 			clf=false;
 			MostrarCARRETERA();
 			printf("\x1b[02;05HDocumentos recogidos: %d", documentos);
@@ -296,16 +313,16 @@ int main() {
 			//Show sprites and run sprite move function
 			movimientoSoldado(&soldado);
 			respawnDocumento(&documento_sprite);
-
 		}
 
-		if(estado==FINAL && clf==false){
+		if(estado==FINAL && clf==false)
+		{
 			consoleClear();
 			clf=true;
 		}
 
-		
-		if(estado==FINAL && clf==true){
+		if(estado==FINAL && clf==true)
+		{
 			soldado.x = 1000;
 			documento_sprite.x = 3000;
 			cl=false;
@@ -316,7 +333,8 @@ int main() {
 			respawnDocumento(&documento_sprite);
 		}
 		
-		if (collisionDetected(documento_sprite, soldado)) {
+		if (collisionDetected(documento_sprite, soldado)) 
+		{
 			documentos++;
 			// move the document to a random place but within the screen, dimensions to which I have subtracted the dimension
 			//of the document so that it is not generated outside and you cannot collect it
@@ -324,21 +342,8 @@ int main() {
 			documento_sprite.y = rand() % (192-16) + 1; 
 		}
 
-		
 		//These functions are needed to update the sprites and the upper screen to be able to see them above the background.
 		swiWaitForVBlank();
 		oamUpdate(&oamMain);
-		
-		
 	}
-	
 }
-
-
-
-
-
-
-
-    
-
